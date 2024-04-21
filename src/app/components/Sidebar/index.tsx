2100909f7400cca126aa91ac7224ac1300811f59
juniordbz <franciscojunior02@hotmail.com.br>
2024-04-21 20:33:14 -0300

    create new searcInput with composition pattern

--- src/app/components/Sidebar/index.tsx
+++ src/app/components/Sidebar/index.tsx
@@ -1,22 +1,20 @@
-import { Search } from 'lucide-react'
 import { Logo } from './logo'
 import { MainNavigation } from './MainNavigation'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
+import { InputControl, InputPrefix, InputRoot } from '../Input'
+import { Search } from 'lucide-react'
 
 export function Sidebar() {
   return (
     <aside className="flex flex-col gap-6 border-r border-zinc-200 px-5 py-8">
       <Logo />
-
-      <div className="mx-1 flex w-full items-center gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm">
-        <Search className="size-5 text-zinc-500" />
-        <input
-          className="flex-1  border-0 bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
-          type="text"
-          placeholder="Search"
-        />
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="size-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl placeholder="Search" />
+      </InputRoot>
 
       <MainNavigation />
 
