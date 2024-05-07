235550360cc8179c51b1c8472d38d3af94f7abdb
juniordbz <franciscojunior02@hotmail.com.br>
2024-05-06 22:29:31 -0300

    open sidebar mobile

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,27 +1,43 @@
+'use client'
+
 import { Logo } from './logo'
 import { MainNavigation } from './MainNavigation'
 import { UsedSpaceWidget } from './UsedSpaceWidget'
 import { Profile } from './Profile'
 import { InputControl, InputPrefix, InputRoot } from '../Input'
-import { Search } from 'lucide-react'
+import { Menu, Search } from 'lucide-react'
+import * as Collapsible from '@radix-ui/react-collapsible'
+import { Button } from '../Button'
 
 export function Sidebar() {
   return (
-    <aside className="fixed inset-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white px-5 py-4 lg:relative lg:inset-auto lg:w-80 lg:border-r lg:py-8">
-      <Logo />
-      <InputRoot>
-        <InputPrefix>
-          <Search className="size-5 text-zinc-500" />
-        </InputPrefix>
-        <InputControl placeholder="Search" />
-      </InputRoot>
+    <Collapsible.Root className="fixed left-0 right-0 top-0 z-20 flex flex-col gap-6 border-b border-zinc-200 bg-white px-5 py-4 data-[state=open]:bottom-0 lg:inset-auto  lg:w-80 lg:border-r lg:py-8 lg:data-[state=closed]:bottom-0">
+      <div className="flex items-center justify-between">
+        <Logo />
+        <Collapsible.Trigger asChild className="lg:hidden">
+          <Button variant="ghost">
+            <Menu className="size-6 " />
+          </Button>
+        </Collapsible.Trigger>
+      </div>
+      <Collapsible.Content
+        forceMount
+        className="flex flex-1 flex-col gap-6 data-[state=closed]:hidden lg:data-[state=closed]:flex "
+      >
+        <InputRoot>
+          <InputPrefix>
+            <Search className="size-5 text-zinc-500" />
+          </InputPrefix>
+          <InputControl placeholder="Search" />
+        </InputRoot>
 
-      <MainNavigation />
+        <MainNavigation />
 
-      <UsedSpaceWidget />
+        <UsedSpaceWidget />
 
-      <div className="h-px bg-zinc-200" />
-      <Profile />
-    </aside>
+        <div className="h-px bg-zinc-200" />
+        <Profile />
+      </Collapsible.Content>
+    </Collapsible.Root>
   )
 }
